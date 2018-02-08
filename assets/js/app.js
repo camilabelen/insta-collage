721d636f9b7158ac31180a7f601a7aff147fc2d6
María Paz Thompson <m.paz.thompson@gmail.com>
2018-02-07 22:25:17 -0300

    Agregando cambios página inicial

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,28 +1,27 @@
 $(document).ready(function() {
 
-  $('.signin').click(function(){
+  $('.signin').click(function() {
     // guardando datos de los inputs        
     var user = $('#user').val();
-    var password = $('#pass').val();      
+    var password = $('#pass').val();
 
     if (user === '' || password === '' || user === '' && password === '') {
-      $('.replaceable').append('<div class=" wrng alert alert-warning" role="alert"> Debes ingresar un usuario y contraseña</div>')
-    } 
+      $('.replaceable').append('<div class="text-center wrng alert alert-warning" style="font-size: 2.2vh" role="alert"> Debes ingresar un usuario y contraseña.</div>')
+    }
     if ($('#pass').val().length < 6 && typeof password !== 'number') {
-      $('.replaceable').append('<div class=" wrng alert alert-warning" role="alert"> La contraseña debe ser de mínimo 6 números</div>')
+      $('.replaceable').append('<div class="wrng alert text-center alert-warning" style="font-size: 2.2vh" role="alert"> La contraseña debe ser de mínimo 6 números.</div>')
     } else {
-    // borrar advertencia
-    $('.wrng').remove();
-    // setItem guarda datos en localStorage
-    localStorage.setItem("User", user);
-    localStorage.setItem("Password", password);
+      // borrar advertencia
+      $('.wrng').remove();
+      // setItem guarda datos en localStorage
+      localStorage.setItem("User", user);
+      localStorage.setItem("Password", password);
 
-    $('#user').val('');
-    $('#pass').val('');
+      $('#user').val('');
+      $('#pass').val('');
 
-    console.log(localStorage.getItem('User'), localStorage.getItem('Password'));
-    window.location.href= "dnd.html";
+      console.log(localStorage.getItem('User'), localStorage.getItem('Password'));
+      window.location.href = "dnd.html";
     }
   });
-});  
- 
+});
